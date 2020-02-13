6f879eb405a10bd01315837922ff9d5beb275fbe
ssatel <saulo.satel@gmail.com>
2020-02-13 18:14:48 -0300

    Aula 10 Primeiro Programa

---
+++ Primeiro/Primeiro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // possui as classe basicas para operar
+
+namespace Primeiro // organização dos arquivos
+{
+    class Program // para cada classe um arquivo, melhor prativa
+    {
+        static void Main(string[] args) /// entry point
+        {
+            Console.WriteLine("Hello World!"); // dependecia do system
+        }
+    }
+}
---
+++ Primeiro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primeiro //existe para organização do arquivos
+{
+    class Program // a classe por boa pratica deve ser equivalente ao nome do arquivo
+    {
+        static void Main(string[] args) //entry point 
+        {
+            Console.WriteLine("Hello World!"); // depencia do using
+        }
+    }
+}
