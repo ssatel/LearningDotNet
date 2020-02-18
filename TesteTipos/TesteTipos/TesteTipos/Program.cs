058319151b8dfb79c1c7be09759a5c1631a02404
ssatel <saulo.satel@gmail.com>
2020-02-18 13:41:05 -0300

    Andamento ate a data

---
+++ TesteTipos/TesteTipos/TesteTipos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TesteTipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte n1 = 127;
+
+            n1++;
+            n1++;
+
+
+            Console.WriteLine(n1);
+        }
+    }
+}
