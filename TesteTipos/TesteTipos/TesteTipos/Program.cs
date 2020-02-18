a12113331ce03443d137328b03068d16a06033f7
ssatel <saulo.satel@gmail.com>
2020-02-18 14:43:49 -0300

    Exercicio 18 Saida de dados

--- TesteTipos/TesteTipos/TesteTipos/Program.cs
+++ TesteTipos/TesteTipos/TesteTipos/Program.cs
@@ -1,18 +1,37 @@
-﻿using System;
+﻿using System; // entrada das dependencias
+using System.Globalization; // permitir utilizar invariantculter, trocar ponto por virla no separador
 
-namespace TesteTipos
+namespace ConsoleApplication2 // somente para organizar o arquivo
 {
-    class Program
+    class Program // Classe utilizar Pascal Case
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // entry point do programa
         {
-            sbyte n1 = 127;
 
-            n1++;
-            n1++;
+            double x = 10.35784; // maior que float que é o tipo primitivo
+            int y = 32; 
+            string z = "Maria"; //string é um tipo contruido
+            char w = 'F'; //tipoprimitigo 
 
+            Console.Write("Olá mundo!"); // o line a linha não é quebrada
+            Console.WriteLine("Bom dia!"); // line no fim do comando quebra a linha
+            Console.WriteLine("Até mais.");
 
-            Console.WriteLine(n1);
+            Console.WriteLine();
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2")); //F2 determinar que o float sera arredondado em duas casas
+            Console.WriteLine(x.ToString("F4")); // arredondamento em duas casas
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture)); // arredondamento em duas casas com virvula no lugar do ponto
+
+            Console.WriteLine();
+            Console.WriteLine("RESULTADO = " + x);
+            Console.WriteLine("O valor do troco é " + x + " reais"); //Concatenação 
+            Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+
+            Console.WriteLine();
+            Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
+
+            Console.ReadLine();
         }
     }
 }
