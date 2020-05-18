9c2b0ef53610b3c64d240c09c514491944a330f5
SATEL-SC <saulo.satel@sa.corp.airliquide.com>
2020-05-18 12:39:45 -0300

    Curso

---
+++ CalculoContrato/CalculoContrato/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculoContrato.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ CalculoContrato/CalculoContrato/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculoContrato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
