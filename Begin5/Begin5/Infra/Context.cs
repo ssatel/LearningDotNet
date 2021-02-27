80476bf87b082494cb4b710af06069026e39a215
Saulo Satel <saulo.satel@gmail.com>
2021-02-27 16:58:40 -0300

    Antes de tentar relacionar usuario e conta e eliminar contextos individuais :)

---
+++ Begin5/Begin5/Infra/Context.cs
@@ -0,0 +1,40 @@
+﻿using Begin5.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Begin5.Infra
+{
+    public class Context : DbContext
+    {
+        public DbSet<Conta> Conta {get;set;}
+
+        public DbSet<Usuario> Usuario { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=new_schema;user=root;password=ml@Satel2014");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Conta>(entity =>
+            {
+                entity.HasKey(c => c.NumeroConta);
+                entity.Property(c => c.NumeroConta).IsRequired();
+
+            });
+
+            modelBuilder.Entity<Usuario>(entity => 
+            {
+                entity.HasKey(u => u.UserID);
+                entity.Property(u => u.UserID).IsRequired();
+
+            });
+        }
+    }
+}
